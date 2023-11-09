b6d30113cbf51cc400a6fb66ae257d0f68e29bda
sydneylipar <sydneylipar21@gmail.com>
2023-11-09 13:14:43 -0600

    Added gold, silver, bronze colors to leaderboard

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
+import { styled } from '@mui/material/styles';
 
 const columns = [
     {
@@ -58,32 +59,46 @@ const columns = [
       },
   ];
 
-class Leaderboard extends React.Component {
+  const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+    '& .first': {
+      backgroundColor: '#F0cf59',
+    },
+    '& .second': {
+      backgroundColor: '#E9e8e1',
+    },
+    '& .third': {
+      backgroundColor: '#E1c190',
+    },
+  }));
+  
+  class Leaderboard extends React.Component {
     render() {
         let gridRows = this.props.activeData.map(row => 
             {return {id: row.Player.Rk, name: row.Player.Player, age: Number(row.Player.Age), goals: Number(row.Performance.Gls),
                 assists: Number(row.Performance.Ast), team: row.Player.Squad, position: row.Player.Pos, 
                 totalPlaytime: Number(row['Playing Time'].Min.replace(',', '')), yellowCards: Number(row.Performance.CrdY),
                 redCards: Number(row.Performance.CrdR)}});
-
-        return (<DataGrid
+  
+      return (
+        <div style={{ height: 400, width: '100%' }}>
+          <StripedDataGrid
             rows={gridRows}
             columns={columns}
             sortingOrder={['desc', 'asc']}
-            initialState={{
-                pagination: {
-                paginationModel: { page: 0, pageSize: 10 },
-                },
-                sorting: {
-                sortModel: [
-                    {
-                    field: 'goals',
-                    sort: 'desc',
-                    },
-                ],
-                },
-            }}
             pageSizeOptions={[5, 10]}
-            />)
+            getRowClassName={(params) =>{
+              if (params.indexRelativeToCurrentPage === 0) {
+                return 'first'; // Apply gold color to the first row
+              } else if (params.indexRelativeToCurrentPage === 1) {
+                return 'second'; // Apply silver color to the second row
+              } else if (params.indexRelativeToCurrentPage === 2) {
+                return 'third'; // Apply bronze color to the third row
+              }
+            }}
+          />
+        </div>
+      );
     }
-} export default Leaderboard;
+  }
+  
+  export default Leaderboard;
